2a0233ce5d781c81e2ef5234b41f71da80ed52f1
web-tech-surgu <95065223+web-tech-surgu@users.noreply.github.com>
2022-04-02 14:16:51 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>лабароторная работа 6.Блочная верстка сайта</title>
+  <link rel="stylesheet" type="text/css" href="stile.css" />
+  </head>
+  <body>
+    <div id=" cantainer">
+      блок, содержащий список категорий
+      </div id=" container:>
+        <div id=" objects">
+          Блок, содержащий список блоков
+            </div>
+        </div>
+  </body>
+  <html>
